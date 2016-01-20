292de37fc796e8f544d019f6191a0da901ed69fc
Michael Ben-Ami <mike.benami@gmail.com>
2016-01-20 00:41:03 -0500

    sphinx ext napoleon

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ if not on_rtd:
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
-    'sphinxcontrib.napoleon'
+    'sphinxc.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
